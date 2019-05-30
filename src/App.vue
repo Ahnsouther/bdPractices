c1240d7cff631bfdf238866882d6248be9944d38
aihaonan <aihaonan89@qq.com>
2019-05-31 00:39:52 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <template>
   <div id="app">
-<<<<<<< HEAD
     <layout></layout>
   </div>
 </template>
@@ -29,32 +28,5 @@ body {
   height: 100%;
   box-sizing: border-box;
   position: absolute; // 解决外边框合并问题
-=======
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
-</template>
-
-<style lang="less">
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-#nav {
-  padding: 30px;
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
->>>>>>> cd5d0ee1dcee0f0ee0414384d660203b62e3c722
 }
 </style>
